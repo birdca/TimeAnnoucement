07e72e18faa8423e7d1e4feef674e9c2d6917cb4
Ca Chen <kiancaca@gmail.com>
2020-09-08 13:14:22 +0800

    make hour between 1-12

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ const CronJob = require('cron').CronJob;
 const job = new CronJob('0 0 * * * *', () => {
   const audioSpeaker = document.getElementById('speak');
   const hr = new Date(Date.now()).getHours();
+  const hour12 = hr ? hr % 12 : 12;
 
-  audioSpeaker.src = `https://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&tl=en&q=${hr}+o%27clock`;
+  audioSpeaker.src = `https://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&tl=en&q=${hour12}+o%27clock`;
   const promise = audioSpeaker.play();
   if (promise !== undefined) {
     promise.then(_ => {
