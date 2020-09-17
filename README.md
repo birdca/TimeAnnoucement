8fc2375b58405f90c84996dae353692cf00aca60
Ca Chen <kiancaca@gmail.com>
2020-09-17 13:37:25 +0800

    1. add time expression 2. time annoucement per half hour

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # 報時小工具
-需要一個自動報時的工具就做了，目前是每小時報一次。
+需要一個自動報時的工具就做了，目前是每半小時報一次。
+```
+$ npm i
+$ npm run build
+
+// 沒有申請 [translate](https://cloud.google.com/translate/docs/setup) 會 404, 因此直接跑這個
+$ open dist/index.html
+```
 
 TODO:
 - [ ] 自訂報時區間
+- [x] 英文時間描述
--- src/index.js
+++ src/index.js
@@ -1,11 +1,41 @@
 const CronJob = require('cron').CronJob;
 
-const job = new CronJob('0 0 * * * *', () => {
-  const audioSpeaker = document.getElementById('speak');
-  const hr = new Date(Date.now()).getHours();
-  const hour12 = hr ? hr % 12 : 12;
+const getTimeExpression = (hh, mm) => {
+  const hour12 = !hh || hh == 12 ? 12 : hh % 12;
+  let first, middle, last;
 
-  audioSpeaker.src = `https://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&tl=en&q=${hour12}+o%27clock`;
+  if (mm < 10){
+    first = hour12;
+    middle = 'o';
+    last = mm == 0 ? 'clock' : mm;
+  } else if (mm == 15 || mm == 45) {
+    first = 'quarter';
+    middle = mm == 15 ? 'past' : 'to';
+    last = mm == 15 ? hour12 : hour12 + 1;
+  } else if (mm < 30) {
+    first = mm;
+    middle = 'past';
+    last = hour12;
+  } else if (mm == 30) {
+    first = 'half';
+    middle = 'past';
+    last = hour12;
+  } else {
+    first = 60 - mm;
+    middle = 'to';
+    last = 1 + hour12;
+  }
+
+  return `https://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&tl=en&q=${first}%20${middle}%20${last}`;
+}
+
+const job = new CronJob('0 */30 * * * *', () => {
+  const audioSpeaker = document.getElementById('speak');
+  const getNow = Date.now();
+  const hh = new Date(getNow).getHours();
+  const mm = new Date(getNow).getMinutes();
+
+  audioSpeaker.src = getTimeExpression(hh, mm);
   const promise = audioSpeaker.play();
   if (promise !== undefined) {
     promise.then(_ => {
